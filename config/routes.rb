cfa5e8645f0224e4485141c2d6709eedeed233a1
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-29 01:39:33 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'sessions#new'
-  resources :secrets, only: :show
+  #resources :secrets, only: :show
+  get '/secret' => 'secrets/show'
   resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
