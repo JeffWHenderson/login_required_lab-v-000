2428fe6765c0880963ecf1f567e6f4b6a64e3e46
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-29 01:34:18 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -4,9 +4,12 @@ class SecretsController < ApplicationController
 
   def show
     
-    "should redirect to login if you're not logged in"
 
-    "should show you the secret if you're logged in"
+  end
+
+  private
+  def require_login
+    return head(:forbidden) unless session.include? :user_id    
   end
 
 end
