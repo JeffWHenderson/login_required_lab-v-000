1b652927987a8489f7716cf7e7081300e804c5eb
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-29 01:26:28 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,10 @@
 class SecretsController < ApplicationController
 
   def show
+    
+    "should redirect to login if you're not logged in"
+
+    "should show you the secret if you're logged in"
   end
 
 end
